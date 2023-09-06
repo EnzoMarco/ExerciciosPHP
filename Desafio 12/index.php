536937bfd26f03e5fbc46984985e46366435b788
Enzo Marco <116755774+EnzoMarco@users.noreply.github.com>
2023-09-05 23:06:31 -0300

    comecei o Desafio 12

---
+++ Desafio 12/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 12</title>
+</head>
+<body>
+    <header>
+        <h1>Reajustador de Preços</h1>
+    </header>
+    <main>
+        <label for="">Preço do Produto (R$)</label>
+        <input type="number">
+        <label>Qual será o percentual de reajuste?()</label>
+        <input type="range" min="0" max="100">
+        <input type="submit">
+    </main>
+    <footer></footer>
+</body>
+</html>
