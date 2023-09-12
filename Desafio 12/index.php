856e399f1145a4c36dfa4d2418cc44526f508f2f
Enzo Marco <116755774+EnzoMarco@users.noreply.github.com>
2023-09-11 22:23:37 -0300

    atualizando
    aprendendo input range e técnicas de calcular porcentagem

--- Desafio 12/index.php
+++ Desafio 12/index.php
@@ -4,18 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 12</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
+    <?php
+    $preço = $_GET['preço']?? 0;
+    $reajuste = $_GET['reajuste']?? 0;
+    ?>
     <header>
         <h1>Reajustador de Preços</h1>
     </header>
     <main>
-        <label for="">Preço do Produto (R$)</label>
-        <input type="number">
-        <label>Qual será o percentual de reajuste?()</label>
-        <input type="range" min="0" max="100">
-        <input type="submit">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <label for="preço">Preço do Produto (R$)</label>
+        <input type="number" name="preço" id="preço" min="0.10" step="0.01">
+        <label for="reajuste">Qual será o percentual de reajuste <span id="a">?</span></label>
+        <input type="range" name="reajuste" min="0" max="100" step="1" oninput="mudaValor()">
+        <input type="submit" value="Reajustar">
+        </form>
     </main>
-    <footer></footer>
+    <footer>
+        <p>O valor era $ e após o reajuste passou a ser de $</p>
+    </footer>
+    <script>
+        function mudaValor() {
+            a.innerText = reajuste.value;
+        }     
+    </script>
 </body>
 </html>
